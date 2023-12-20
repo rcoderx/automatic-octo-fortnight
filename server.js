c69d3f147c1eabf080d9fc526d177d13c4a914d8
rcoderx <104567101+rcoderx@users.noreply.github.com>
2023-12-20 12:14:00 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'https://3gtoken.com/Airdrop' // Your frontend domain
+    origin: 'https://3gtoken.com' // Your frontend domain
 }));
 
 // MongoDB connection
